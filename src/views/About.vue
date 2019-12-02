9fc02d7f0827a058bce175d556ff69f07d35fe5e
matsumana07384 <manami.m.227@outlook.jp>
2019-12-03 00:05:27 +0900

    modify about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+       <router-link to="done">
+          <button type="button" name="done" value="done">done</button>
+       </router-link>
   </div>
 </template>
