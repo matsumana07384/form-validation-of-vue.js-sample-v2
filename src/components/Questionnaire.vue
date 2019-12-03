8b4509da2c63d9d3ab31d3ae311ce9d69d23769d
matsumana07384 <manami.m.227@outlook.jp>
2019-12-04 00:15:45 +0900

    wip add component

---
+++ src/components/Questionnaire.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <p><b>{{ title }}</b></p>
+</div>
+<!-- ここから追加 -->
+
+<!-- ここまで追加 -->
+</template>
+
+<script>
+export default {
+  props: {
+      title : String
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-       <router-link to="done">
-          <button type="button" name="done" value="done">done</button>
-       </router-link>
+    <Questionnaire title="アンケート"></Questionnaire>
+    <router-link to="done">
+      <button type="button" name="done" value="done">done</button>
+    </router-link>
   </div>
 </template>
+
+<script>
+import Questionnaire from '@/components/Questionnaire.vue'
+
+export default {
+  name: 'about',
+  components: {
+    Questionnaire
+  }
+}
+</script>
