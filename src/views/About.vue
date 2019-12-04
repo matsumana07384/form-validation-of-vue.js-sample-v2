358972d831a9779e426640d043c26c6717c215d2
matsumana07384 <manami.m.227@outlook.jp>
2019-12-04 23:31:13 +0900

    remove unused button

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
     <Questionnaire title="アンケート"></Questionnaire>
-    <router-link to="done">
-      <button type="button" name="done" value="done">done</button>
-    </router-link>
   </div>
 </template>
 
